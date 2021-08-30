edb7e61209a4515e0d2b20297f46922d2d1ea017
ywoo <yunzi.woo@gmail.com>
2021-08-31 07:33:26 +0900

    2021-08-31

--- Ch 01. C언어 기반의 C++/Ex 01-1.cpp
+++ Ch 01. C언어 기반의 C++/Ex 01-1.cpp
@@ -1,7 +1,44 @@
 #include <iostream>
+#include <cstring>
+using namespace std;
+
+const int NameLen = 20;
+
+void ShowMenu(void){
+
+}
+
+void MakeAccount(void){
+
+}
+
+void DepositMoney(void){
+
+}
+
+void WithdrawMoney(void){
+
+}
+
+
 
 int main(void){
     
-    std::""
+    int menu;
+
+    std::cout<<"----- MENU -----" <<std::endl;
+    std::cout<<"1. 계좌 개설" <<std::endl;
+    std::cout<<"2. 입금" <<std::endl;
+    std::cout<<"3. 출금" <<std::endl;
+    std::cout<<"4. 계좌정보 전체 출력" <<std::endl;
+    std::cout<<"5. 프로그램 종류"<<std::endl;
+    std::cout<<"선택 : ";
+    std::cin >> menu;
+
+
     return 0;
+}
+
+Class id = new Bank{
+
 }
